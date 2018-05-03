89e0284b3ac5ba1cf29f8bd2ca94e1bc7bc94cbf
HanlongLiao <liamliaohl@gmail.com>
2018-05-04 05:34:16 +0800

    Fri 2018/5/4 13:34CST finsh host

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,10 @@
-import flake8
-import requests
 import hashlib
 import json
 
 from time import time
 from uuid import uuid4
+
+from flask import Flask
 
 
 class Blockchain(object):
@@ -16,12 +16,12 @@ class Blockchain(object):
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
-        '''
+        """
          Create a new block, and adds it to chain
         :param proof: <int> The proof given by the proof of work algorithm
         :param previous_hash: (Optional) <str> Hash of previous block
         :return: <dict> New block
-        '''
+        """
 
         block = {
             'index': len(self.chain) + 1,
@@ -38,13 +38,13 @@ class Blockchain(object):
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        '''
+        """
         Create a new transaction to go into the next mined Block
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the recipient
         :param amount: <int> Amount
         :return: <int> The index of the block that will hold this transaction
-        '''
+        """
 
         self.current_transaction.append({
             'sender': sender,
@@ -56,11 +56,11 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        '''
+        """
         Create a SHA-256 hash of a block
         :param block: <dict> Block
         :return: <str>
-        '''
+        """
 
         # we must make sure that the dictionary is ordered, or we'll have inconsistent hashes
         block_string = json.dump(block, sort_keys=True).encode()
@@ -68,8 +68,66 @@ class Blockchain(object):
 
     @property
     def last_block(self):
-       return self.chain[-1]
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple proof of work algorithm
+        - Find a number p', such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+       Validates the proof: Does hash(last_proof, proof) contain 4 leading zeroes
+        :param last_proof:<int> Previous proof
+        :param proof:<int> Current proof
+        :return:<bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
+
+
+# Instantiate out Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
-    pass
+    app.run(host='0.0.0.0', port=5000)
